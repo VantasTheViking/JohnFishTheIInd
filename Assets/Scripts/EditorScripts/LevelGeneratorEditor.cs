333090e858b74f15a4ec1ef5977d4be5b861eb3d
VantasTheViking <72925846+VantasTheViking@users.noreply.github.com>
2025-04-19 22:10:20 -0400

    new rooms

---
+++ Assets/Scripts/EditorScripts/LevelGeneratorEditor.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LevelGenerator))]
+public class LevelGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        LevelGenerator levelGenerator = (LevelGenerator)target;
+
+        if(GUILayout.Button("Generate Level"))
+        {
+            levelGenerator.GenerateLevel();
+        }
+    }
+}
--- Assets/Scripts/ProceduralGenerator/LevelGenerator/LevelGenerator.cs
+++ Assets/Scripts/ProceduralGenerator/LevelGenerator/LevelGenerator.cs
@@ -5,15 +5,46 @@ public class LevelGenerator : MonoBehaviour
 
     [SerializeField, Min(1), Tooltip("Number of Rooms & Connectors to be Generator. *Not including Spawn & Objective")] 
     int _levelSize = 1;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    [SerializeField]
+    MapType _mapType;
+
+
+
+    public void GenerateLevel()
     {
-        
+        validateMapData();
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+
+    void validateMapData()
     {
-        
+        foreach(GameObject room in _mapType.IntermediateRooms)
+        {
+            if (room.GetComponent<Room>() == null)
+            {
+                throw new System.Exception($"{room.name} is missing a Room Component");
+            }
+        }
+
+        foreach (GameObject room in _mapType.Connector)
+        {
+            if (room.GetComponent<Room>() == null)
+            {
+                throw new System.Exception($"{room.name} is missing a Room Component");
+            }
+        }
+
+        if (_mapType.Spawn.GetComponent<Room>() == null)
+        {
+            throw new System.Exception($"{_mapType.Spawn.name} is missing a Room Component");
+        }
+
+        if (_mapType.Objective.GetComponent<Room>() == null)
+        {
+            throw new System.Exception($"{_mapType.Objective.name} is missing a Room Component");
+        }
     }
 }

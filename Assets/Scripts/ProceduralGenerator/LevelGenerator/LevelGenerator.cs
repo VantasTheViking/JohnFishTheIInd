b09c3f71081417b940202397e7c28737b616b6f4
VantasTheViking <72925846+VantasTheViking@users.noreply.github.com>
2025-04-22 17:05:59 -0400

    added room detection with spawning

--- Assets/Scripts/ProceduralGenerator/LevelGenerator/LevelGenerator.cs
+++ Assets/Scripts/ProceduralGenerator/LevelGenerator/LevelGenerator.cs
@@ -1,3 +1,6 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
@@ -9,15 +12,61 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField]
     MapType _mapType;
 
+    [SerializeField]
+    List<GameObject> rooms = new List<GameObject>();
+
+
+    private void Start()
+    {
+
+        SpawnRoom(_mapType.Spawn, transform.position, transform.rotation);
+        if (CheckSpaceForRoomSpawning(_mapType.IntermediateRooms[0], transform.position, transform.rotation)) { Debug.Log("CANSPAWN"); SpawnRoom(_mapType.IntermediateRooms[0], transform.position, transform.rotation); } else { Debug.Log("CANTSPAWN"); }
+
+    }
 
 
     public void GenerateLevel()
     {
         validateMapData();
+
+        
     }
 
+    bool CheckSpaceForRoomSpawning(GameObject roomPrefab, Vector3 spawnPosition, Quaternion spawnRotation)
+    {
+        
+        Bounds allPrefabBounds = GetRoomBounds(roomPrefab);
+        Vector3 halfExtents = allPrefabBounds.extents;
+        Collider[] hits = Physics.OverlapBox(spawnPosition + allPrefabBounds.center, halfExtents, spawnRotation);
+        
+        if(hits.Length == 0 ) {return true;} else {return false;}
 
+    }
 
+    void SpawnRoom(GameObject roomPrefab, Vector3 spawnPosition, Quaternion spawnRotation)
+    {
+        GameObject spawnedRoom = Instantiate(roomPrefab,spawnPosition,spawnRotation);
+        rooms.Add(spawnedRoom);
+    }
+
+    /// <summary>
+    /// Returns the bounds of the room using all the colliders of the prefab.
+    /// </summary>
+    Bounds GetRoomBounds(GameObject roomPrefab)
+    {
+        Collider[] colliders = roomPrefab.GetComponentsInChildren<Collider>();
+
+        //If there are no colliders return nothing
+        if (colliders.Length == 0) { return new Bounds(roomPrefab.transform.position, Vector3.zero); }
+
+        Bounds allBounds = colliders[0].bounds;
+        for (int i = 1; i < colliders.Length; i++)
+        {
+            allBounds.Encapsulate(colliders[i].bounds);
+        }
+
+        return allBounds;
+    }
 
     void validateMapData()
     {
--- Assets/Scripts/ProceduralGenerator/Room/Room.cs
+++ Assets/Scripts/ProceduralGenerator/Room/Room.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Bounds combinedBounds;
+
+    void Awake()
     {
-        
+
+
+        CalculateCombinedBounds();
     }
 
-    // Update is called once per frame
-    void Update()
+    void CalculateCombinedBounds()
     {
-        
+        Collider[] colliders = GetComponentsInChildren<Collider>();
+        if (colliders.Length == 0)
+        {
+            combinedBounds = new Bounds(transform.position, Vector3.zero);
+            return;
+        }
+
+        combinedBounds = colliders[0].bounds;
+        for (int i = 1; i < colliders.Length; i++)
+        {
+            combinedBounds.Encapsulate(colliders[i].bounds);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireCube(combinedBounds.center, combinedBounds.size);
     }
 }
